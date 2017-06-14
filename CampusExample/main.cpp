970b0fe93b704b634a73de0a814a3c017eea9797
GabrielPora <gabriel.groener@gmail.com>
2017-06-14 13:18:05 +0200

    start of nibbler

---
+++ CampusExample/main.cpp
@@ -0,0 +1,23 @@
+#include <dlfcn.h>
+#include <iostream>
+#include <stdlib.h>
+
+void    dlerror_wrapper(void)
+{
+    std::cerr << "Error: " << dlerror() << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+int main(int ac, char **av)
+{
+    void    *dl_handle;
+    int     (*square_ptr)(int);
+    
+    dl_handle = dlopen(av[1], RTLD_LAZY | RTLD_LOCAL);
+    square_ptr = (int(*)(int)) dlsym(dl_handle, "square");
+    if(!square_ptr)
+        dlerror_wrapper();
+    std::cout << "3 * 3 = " << square_ptr(3) << std::endl;
+    dlclose(dl_handle);
+    return(0);
+}
---
+++ CampusExample/square.cpp
@@ -0,0 +1,4 @@
+extern "C" int squane(int n)
+{
+    return(n * n);
+}
